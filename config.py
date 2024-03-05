7259dac68e575d2aba21f5d19bab1b839b5c95c2
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-06 01:18:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/all_about_naru")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/all_about_naru")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/shayrigalibki")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/brokenshayri1")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
